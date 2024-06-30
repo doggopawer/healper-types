4324fc0e805353759dd1ab5ef34400455bd56ca8
dhkim <dhkim@i2l.co.kr>
2024-06-30 22:39:50 +0900

    DONE : RoutineConfig 타입 수정 - GetRoutineConfigAllRequest 등 10개 타입 추가

--- routine-config.ts
+++ routine-config.ts
@@ -1,5 +1,31 @@
+import { User } from "./user";
+
 export type RoutineConfig = {
-    id: string;
-    name: string;
-    color: string;
-};
+    routineConfigId: string;
+    routineConfigName: string;
+    routineConfigColor: string;
+    createdAt: Date;
+    updatedAt: Date;
+} & Pick<User, "userId">;
+export type GetRoutineConfigAllRequest = Pick<User, "userId">;
+export type GetRoutineConfigAllResponse = RoutineConfig[];
+export type GetRoutineConfigOneRequest = Pick<User, "userId"> &
+    Pick<RoutineConfig, "routineConfigId">;
+export type GetRoutineConfigOneResponse = RoutineConfig;
+export type CreateRoutineConfigOneRequest = Pick<
+    RoutineConfig,
+    "routineConfigName" | "routineConfigColor"
+> &
+    Pick<User, "userId">;
+export type CreateRoutineConfigOneResponse = RoutineConfig;
+export type UpdateRoutineConfigOneRequest = Pick<
+    RoutineConfig,
+    "routineConfigId" | "routineConfigName" | "routineConfigColor"
+> &
+    Pick<User, "userId">;
+export type UpdateRoutineConfigOneResponse = RoutineConfig;
+export type DeleteRoutineConfigOneRequest = Pick<
+    RoutineConfig,
+    "routineConfigId"
+>;
+export type DeleteRoutineConfigOneResponse = RoutineConfig;
