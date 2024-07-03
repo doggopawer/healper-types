d54ab2ff080f23d9e796867ad88b0887ac444520
dhkim <dhkim@i2l.co.kr>
2024-07-03 22:11:31 +0900

    feat: SetConfig 타입 생성

---
+++ set-config.ts
@@ -0,0 +1,10 @@
+export type SetConfig = {
+    id: number;
+    order: number;
+    weight: number;
+    rep: number;
+    restSec: number;
+    createdAt: Date;
+    updatedAt: Date;
+    workoutConfigId: number;
+};
