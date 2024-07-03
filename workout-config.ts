e1e6dc9b41dc1bea8e8abc9bd775565daa1edd9b
dhkim <dhkim@i2l.co.kr>
2024-07-03 22:11:58 +0900

    feat: WorkoutConfig 타입 생성

---
+++ workout-config.ts
@@ -0,0 +1,9 @@
+export type WorkoutConfig = {
+    id: number;
+    name: string;
+    workoutImage: string;
+    type: string;
+    createdAt: Date;
+    updatedAt: Date;
+    routineConfigId: number;
+};
