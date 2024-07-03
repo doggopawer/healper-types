20c9bb511086acd76b21377bf35e7df710dde486
dhkim <dhkim@i2l.co.kr>
2024-07-03 22:11:39 +0900

    feat: SetRecord 타입 생성

---
+++ set-record.ts
@@ -0,0 +1,10 @@
+export type SetRecord = {
+    id: number;
+    order: number;
+    weight: number;
+    rep: number;
+    restSec: number;
+    createdAt: Date;
+    updatedAt: Date;
+    workoutRecordId: number;
+};
