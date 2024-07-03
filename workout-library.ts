03344c6e371ded74c56e6fe16478bf60f0b399a3
dhkim <dhkim@i2l.co.kr>
2024-07-03 22:12:08 +0900

    feat: WorkoutLibrary 타입 생성

---
+++ workout-library.ts
@@ -0,0 +1,10 @@
+export type WorkoutLibrary = {
+    id: number;
+    name: string;
+    workoutImage: string;
+    workoutPart: string;
+    type: string;
+    createdAt: Date;
+    updatedAt: Date;
+    userId: number;
+};
