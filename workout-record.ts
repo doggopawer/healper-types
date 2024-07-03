44ba0b3faa2f5dd64178629643cb1e74cfcbcba6
dhkim <dhkim@i2l.co.kr>
2024-07-03 22:12:17 +0900

    feat: WorkoutRecord 타입 생성

---
+++ workout-record.ts
@@ -0,0 +1,9 @@
+export type WorkoutRecord = {
+    id: number;
+    name: string;
+    workoutImage: string;
+    type: string;
+    createdAt: Date;
+    updatedAt: Date;
+    routineRecordId: number;
+};
